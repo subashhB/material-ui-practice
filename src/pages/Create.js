f5ed657668b0cf814a147165df9a307cf75af18d
Admin <subashbaidya9@gmail.com>
2023-03-21 18:00:01 +0545

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
 
 const Create = () => {
   return (
-    <div>Create Page</div>
+    <Typography
+        variant='h6'
+        component='h2'
+        color='textSecondary'
+    >
+        Create a New Note
+    </Typography>
+
   )
 }
 
