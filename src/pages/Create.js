fcf2acbe0089d93a6fd6c9f986b32f61698ac56b
Admin <subashbaidya9@gmail.com>
2023-03-22 18:24:07 +0545

    Buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import Typography from '@mui/material/Typography';
+import {Button} from '@mui/material';
+import { Container } from '@mui/system';
 
 const Create = () => {
   return (
-    <Typography
-        variant='h6'
-        component='h2'
-        color='textSecondary'
-    >
-        Create a New Note
-    </Typography>
-
+    <Container>
+      <Typography
+          variant='h6'
+          component='h2'
+          color='textSecondary'
+      >
+          Create a New Note
+      </Typography>
+      <Button 
+        onClick={() =>{console.log('You Clicked Me!')}}
+        type='Submit' 
+        color='secondary' 
+        variant='contained'
+      >
+        SUBMIT
+      </Button>
+    </Container>
   )
 }
 
