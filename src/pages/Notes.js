048039010814e861bdda2bd3dce591c006e3ed72
Admin <subashbaidya9@gmail.com>
2023-03-28 07:14:50 +0545

    Using JSON Server

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Notes = () => {
+  const [notes, setNotes] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:8000/notes')
+      .then(res => res.json())
+      .then(data => setNotes(data))
+  },[])
   return (
-    <div>Create Note</div>
+    <div>
+      {notes && notes.map(note=>(
+        <p key={note.id}>{note.title}</p>
+      ))}
+    </div>
   )
 }
 
